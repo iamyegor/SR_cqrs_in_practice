08187fac613abcef72dd63e4d2893a00a5c299c0
yegor <astery227@gmail.com>
2024-04-13 19:02:14 +0300

    A: Create a high-level skeletion of SyncAndResetFlags

--- DbSynchronization/Synchronizers/Students/Repositories/CommandDbStudentRepository.cs
+++ DbSynchronization/Synchronizers/Students/Repositories/CommandDbStudentRepository.cs
@@ -7,6 +7,41 @@ public class CommandDbStudentRepository
 {
     public IReadOnlyList<StudentInCommandDb> GetForSyncAndResetFlags(NpgsqlTransaction transaction)
     {
+        (List<int> studentIds, List<int> enrollmentIds) = LockStudentsAndEnrollments(transaction);
+
+        IReadOnlyList<StudentInCommandDb> students = GetStudentsForSync(studentIds, transaction);
+
+        DeleteSoftDeletedEnrollments(enrollmentIds, transaction);
+        ResetSyncFlags(studentIds, transaction);
+
+        return students;
+    }
+
+    private (List<int> studentIds, List<int> enrollmentIds) LockStudentsAndEnrollments(
+        NpgsqlTransaction transaction
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DeleteSoftDeletedEnrollments(
+        List<int> enrollmentIds,
+        NpgsqlTransaction transaction
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private IReadOnlyList<StudentInCommandDb> GetStudentsForSync(
+        List<int> studentIds,
+        NpgsqlTransaction transaction
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ResetSyncFlags(List<int> studentIds, NpgsqlTransaction transaction)
+    {
         throw new NotImplementedException();
     }
 }
