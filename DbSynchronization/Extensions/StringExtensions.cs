52bb882999594f08da785c208cdf4cd8b8fc0dac
yegor <astery227@gmail.com>
2024-04-15 14:46:05 +0300

    A: Implement ToSnakeCase

--- DbSynchronization/Extensions/StringExtensions.cs
+++ DbSynchronization/Extensions/StringExtensions.cs
@@ -1,9 +1,34 @@
-﻿namespace DbSynchronization.Extensions;
+﻿using System.Text;
+
+namespace DbSynchronization.Extensions;
 
 public static class StringExtensions
 {
     public static string? ToSnakeCase(this string? input)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return input;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] characters = input.ToCharArray();
+
+        stringBuilder.Append(char.ToLower(characters[0]));
+
+        for (int i = 1; i < characters.Length; i++)
+        {
+            if (char.IsUpper(characters[i]))
+            {
+                stringBuilder.Append('_');
+                stringBuilder.Append(char.ToLower(characters[i]));
+            }
+            else
+            {
+                stringBuilder.Append(characters[i]);
+            }
+        }
+
+        return stringBuilder.ToString();
     }
 }
