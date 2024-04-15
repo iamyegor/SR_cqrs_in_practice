0328454177857b442c212ee386e076c899e5ac0b
yegor <astery227@gmail.com>
2024-04-15 12:46:46 +0300

    A: Implement assembly scanner without decorators

--- Api/StartupHelpers.cs
+++ Api/StartupHelpers.cs
@@ -1,12 +1,6 @@
 using System.Reflection;
 using Api.Utils;
-using Logic.Application.Commands.Common;
-using Logic.Application.Commands.Disenroll;
-using Logic.Application.Commands.EditPersonalInfo;
-using Logic.Application.Commands.Enroll;
-using Logic.Application.Commands.Transfer;
 using Logic.Application.Queries.Common;
-using Logic.Application.Queries.GetStudentsList;
 using Logic.Application.Utils;
 using Logic.DAL;
 using Logic.DAL.Repositories;
@@ -37,17 +31,7 @@ public static class StartupHelpers
         var config = TypeAdapterConfig.GlobalSettings;
         config.Scan(Assembly.GetExecutingAssembly());
 
-        builder.Services.AddTransient<ICommandHandler<EnrollCommand>, EnrollCommandHandler>();
-        builder.Services.AddTransient<ICommandHandler<TransferCommand>, TransferCommandHandler>();
-        builder.Services.AddTransient<ICommandHandler<DisenrollCommand>, DisenrollCommandHandler>();
-        builder.Services.AddTransient<
-            ICommandHandler<EditPersonalInfoCommand>,
-            EditPersonalInfoCommandHandler
-        >();
-        builder.Services.AddTransient<
-            IQueryHandler<GetStudentsListQuery, IReadOnlyList<StudentInDb>>,
-            GetStudentsListQueryHandler
-        >();
+        builder.Services.AddCqrsHandlers();
 
         return builder;
     }
