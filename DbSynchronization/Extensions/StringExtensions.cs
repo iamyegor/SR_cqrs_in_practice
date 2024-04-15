ecd6192d2ce1e1e3e4ded67df37eeeaad403ad5a
yegor <astery227@gmail.com>
2024-04-15 14:45:05 +0300

    Q: Implement ToSnakeCase

---
+++ DbSynchronization/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace DbSynchronization.Extensions;
+
+public static class StringExtensions
+{
+    public static string? ToSnakeCase(this string? input)
+    {
+        throw new NotImplementedException();
+    }
+}
